bbdd9419f78cf5710ce566583c62588dda06097b
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-15 13:41:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// Khamzat Uppgift 2.13
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Lön för Abdione");
+		int Abdione = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("Lön för Abditwo");
+		int Abditwo = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("Lön för Abditree");
+		int Abditree = int.Parse(Console.ReadLine());
+
+		Console.WriteLine(" Abdione " + Abdione + "kr" + " Abditwo " + Abditwo + "kr" + " Abditree " + Abditree + "kr");
+
+		Console.WriteLine((Abdione + Abditwo + Abditree) / 3);
+	}
+}
